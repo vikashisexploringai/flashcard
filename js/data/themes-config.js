f4a936d32c35f96f4711117474bc33743b04c5bd
vikashisexploringai <vikashisexploringai@gmail.com>
2025-10-22 09:23:32 +0530

    Create themes-config.js

---
+++ js/data/themes-config.js
@@ -0,0 +1,11 @@
+// js/data/themes-config.js
+const THEMES_CONFIG = {
+    'vocabulary': { displayName: 'Vocabulary' },
+    'tables': { displayName: 'Table' },
+    'indianRulers': { displayName: 'Indian Rulers Timeline' },
+    'atomicNumbers': { displayName: 'Atomic Numbers' },
+    'britishGovernors': { displayName: 'Governor General and Viceroys' },
+    'trignometricEquations': { displayName: 'Trignometric Equations' },
+    'gstSections': { displayName: 'GST Sections' },
+    'antonyms': { displayName: 'Antonyms' },
+};
