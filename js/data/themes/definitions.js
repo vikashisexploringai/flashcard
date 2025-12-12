e4f433baa622a76e5bc50f48ecc77a368824b380
vikashisexploringai <vikashisexploringai@gmail.com>
2025-12-12 21:58:29 +0530

    Add definitions theme with science flashcard

---
+++ js/data/themes/definitions.js
@@ -0,0 +1,13 @@
+const articlesTheme = {
+    id: 'definitions',
+    name: 'Misc Definitions',
+    description: 'Important Definitions',
+    flashcards: [
+        {
+    question: "Science",
+    paragraph: `Science is the systematic study of the natural world through observation and experimentation to build organized knowledge, aiming to understand how and why things work, produce testable explanations, and make predictions about the universe. It's both a process (the scientific method) and the body of knowledge gained from that process, covering everything from atoms to galaxies.`
+}
+    ]
+};
+
+ThemeLoader.registerTheme(articlesTheme);
